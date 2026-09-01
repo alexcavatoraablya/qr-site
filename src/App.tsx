f943e89239f518d4172a270f3ee4db06653c6974
MaksymPlichuk <pilipm449@gmail.com>
2026-09-01 11:55:58 +0300

    register page

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Header from "./components/Header.tsx";
+import LoginPage from "./pages/login/LoginPage.tsx";
+import {Route, Routes} from "react-router";
+import HomePage from "./pages/home/HomePage.tsx";
+import RegisterPage from "./pages/register/RegisterPage.tsx";
+
+function App() {
+
+    return (
+        <>
+            <Header/>
+            <Routes>
+                <Route path={"/"}>
+                    <Route index element={<HomePage/>}/>
+                    <Route path={"login"} element={<LoginPage/>}/>
+                    <Route path={"register"} element={<RegisterPage/>}/>
+                </Route>
+            </Routes>
+        </>
+    )
+}
+
+export default App
---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+    return (
+        <>
+            <div className="max-w-xl mx-auto mt-10 px-4 font-sans">
+                <h1 className="text-2xl font-bold nb-6">Golovna storinka</h1>
+            </div>
+        </>
+    );
+}
+
+export default HomePage;
