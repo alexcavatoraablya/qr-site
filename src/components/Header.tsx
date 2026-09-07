34f83fa7784dda583e391f17f75a5ae618c15cfa
MaksymPlichuk <pilipm449@gmail.com>
2026-09-07 11:54:50 +0300

    validation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,26 @@
+import {Link} from "react-router";
+
 const Header = () => {
     return (
         <>
             <header className="flex items-center justify-between px-6 py-4 border-b border-gray-200">
-                <a href={""} className="text-lg font-bold text-gray-900">
+                <Link to={""} className="text-lg font-bold text-gray-900">
                     31КН-QRCode
-                </a>
+                </Link>
 
                 <div className="flex items-center gap-3">
-                    <a
-                        href={"#"}
+                    <Link
+                        to={"/login"}
                         className="px-4 py-2 rounded-lg bg-indigo-600 hover:bg-indigo-700 text-white text-sm font-medium transition-colors"
                     >
                         Вхід
-                    </a>
-                    <a
-                        href={"#"}
+                    </Link>
+                    <Link
+                        to={"/register"}
                         className="px-4 py-2 rounded-lg border border-gray-300 hover:bg-gray-50 text-sm font-medium transition-colors"
                     >
                         Реєстрація
-                    </a>
+                    </Link>
                 </div>
             </header>
         </>
--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -1,6 +1,27 @@
+import {type SubmitHandler, useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {registerSchema} from "./validate.ts";
+import type {IRegisterType} from "./types.ts";
+
 const RegisterPage = () => {
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: {errors, isDirty}, //якщо є помилки
+    } = useForm<IRegisterType>({
+        resolver: zodResolver(registerSchema),
+        mode: "onChange"
+    });
+
+    const onSubmit: SubmitHandler<IRegisterType> = (data) => {
+        console.log("Валідні дані форми:", data);
+        reset();
+    };
+
     return (
-        <>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div className="flex items-center justify-center px-4 mt-20">
                 <div className="w-full max-w-md p-8 space-y-6 bg-white border border-gray-200 rounded-2xl shadow-sm">
                     <h1 className="text-2xl font-bold text-center text-gray-900">Реєстрація</h1>
@@ -8,8 +29,9 @@ const RegisterPage = () => {
                     <form className="space-y-4">
                             <div>
                                 <label className="block text-sm font-medium text-gray-700 mb-1">Ім'я</label>
-                                <input
-                                    type="First name"
+                                <input  {...register('firstName')}
+                                        //{errors.firstName && <p style={{ color: 'red' }}>{errors.firstName.message}</p>}
+                                    type="Firstname"
                                     required
                                     className="w-full px-4 py-2.5 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none"
                                 />
@@ -17,8 +39,9 @@ const RegisterPage = () => {
 
                             <div>
                                 <label className="block text-sm font-medium text-gray-700 mb-1">Прізвище</label>
-                                <input
-                                    type="Last name"
+                                <input {...register('lastName')}
+                                       //{errors.lastName && <p style={{ color: 'red' }}>{errors.lastName.message}</p>}
+                                    type="Lastname"
                                     required
                                     className="w-full px-4 py-2.5 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none"
                                 />
@@ -26,7 +49,8 @@ const RegisterPage = () => {
 
                         <div>
                             <label className="block text-sm font-medium text-gray-700 mb-1">Email</label>
-                            <input
+                            <input {...register('email')}
+                                   //{errors.email && <p style={{ color: 'red' }}>{errors.email.message}</p>}
                                 type="email"
                                 required
                                 className="w-full px-4 py-2.5 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none"
@@ -35,7 +59,8 @@ const RegisterPage = () => {
 
                         <div>
                             <label className="block text-sm font-medium text-gray-700 mb-1">Пароль</label>
-                            <input
+                            <input {...register('password')}
+                                   //{errors.password && <p style={{ color: 'red' }}>{errors.password.message}</p>}
                                 type="password"
                                 required
                                 className="w-full px-4 py-2.5 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none"
@@ -44,7 +69,8 @@ const RegisterPage = () => {
 
                         <div>
                             <label className="block text-sm font-medium text-gray-700 mb-1">Підтвердження пароля</label>
-                            <input
+                            <input {...register('confirmPassword')}
+                                //{errors.confirmPassword && <p style={{ color: 'red' }}>{errors.confirmPassword.message}</p>}
                                 type="confirmpassword"
                                 required
                                 className="w-full px-4 py-2.5 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none"
@@ -54,14 +80,15 @@ const RegisterPage = () => {
 
                         <button
                             type="submit"
+                            disabled={!isDirty}
                             className="w-full py-2.5 rounded-lg bg-indigo-600 hover:bg-indigo-700 disabled:opacity-60 text-white font-medium transition-colors"
                         >
-                            Увійти
+                            Заруєструватися
                         </button>
                     </form>
                 </div>
             </div>
-        </>
+        </form>
     );
 }
 
